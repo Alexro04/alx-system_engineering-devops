b869a2f0ae84aabc375999383d9c29efbb59f93e
Oluwatomi <ajakatomi@gmail.com>
2023-06-29 23:22:07 +0100

    SSH config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,18 @@
+# Client config file setup
+include stdlib
+
 # SSH client configuration to refuse authentication using a password
-file {'~/.ssh/ssh_config':
-    content => '
-    Host *
-        PasswordAuthentication no
-        IdentityFile ~/.ssh/school
-    '
+file-line {'OffPasswordAuthentication': 
+    ensure => 'present'
+    path => '/etc/ssh/ssh_config'
+    line => '   PasswordAuthentication no'
+    repalce => true
+}
+
+# SSH client configuration to use the private key ~/.ssh/school
+file-line {'UseIdentityFile':
+    ensure => 'present',
+    path => '/etc/ssh/ssh_config'
+    line => '    IdentityFile ~/.ssh/school'
+    repalce => true
 }
